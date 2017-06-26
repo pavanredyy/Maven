41f14e25a8c07a85695cf8e41d691ae995722fef
pavanredyy <pavanredyy@yahoo.com>
2017-06-25 23:21:20 -0700

    Initial Commit of Maven Code

---
+++ src/main/java/GreetUser/Hello/HelloUser.java
@@ -0,0 +1,12 @@
+package GreetUser.Hello;
+
+public class HelloUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello");
+	}
+	
+
+}
---
+++ src/test/java/GreetUser/Hello/HelloTest.java
@@ -0,0 +1,14 @@
+package GreetUser.Hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
